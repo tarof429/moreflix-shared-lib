762d1f89d5b6993b0c795c3bb30700866fdbb555
Taro Fukunaga <tarof429@gmail.com>
2025-02-26 23:13:30 +0000

    Update githubCommit

--- vars/githubCommit.groovy
+++ vars/githubCommit.groovy
@@ -1,8 +1,21 @@
 #!/usr/bin/env groovy
 
 def call() {
-    def branch = env.BRANCH_NAME
-    sh 'git add .'
-    sh 'git commit -m "CI: version bump"'
-    sh "git push origin HEAD:${branch}"
+    withCredentials([
+        string(
+            credentialsId: credentialsId, 
+            variable: 'GIT_PAT')]) {
+        url = url.replace("https://github.com", "https://${GIT_PAT}@github.com")
+
+        echo "Committing changes to ${url}"
+
+        sh("git config --global user.email 'jenkins@release.com'")
+        sh("git config --global user.name 'jenkins'")
+        sh("git remote set-url origin ${url}")
+}
+        def branch = env.BRANCH_NAME
+        sh 'git add .'
+        sh 'git commit -m "CI: version bump"'
+        sh "git push origin HEAD:${branch}"
+    }
 }
